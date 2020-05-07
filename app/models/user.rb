56a9b7fd8ffc61fe50189fa618af3bc125de3baf
koshlendra <koshlendratomar31@gmail.com>
2020-05-07 23:59:25 +0530

    ReadmMe Updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   include Authenticatable
   attr_accessor :with_password
 
-  has_many :transactions, dependent: :destroy
+  has_many :transactions
 
   enum status: [:inactive, :active]
+
 end
