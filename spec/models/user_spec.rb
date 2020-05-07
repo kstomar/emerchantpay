abd8b32fb8b099d79754b27c3b7b98ecfca022b4
koshlendra <koshlendratomar31@gmail.com>
2020-05-07 21:38:15 +0530

    Model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should have_many(:transactions)}
+
+  it "should have status enum" do
+    should define_enum_for(:status).
+        with_values({inactive: 0, active: 1})
+  end
 end
